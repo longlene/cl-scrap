80788712e5c42f0ef2bf60970acab845d9653b58
longlene <longlene@gmail.com>
2013-11-28 11:56:33 +0800

    c:gcd function

---
+++ c/gcd.c
@@ -0,0 +1,20 @@
+inline static long gcd(long x, long y)
+{
+        if (x < 0)
+                x = -x;
+        if (y < 0)
+                y = -y;
+
+        if (x == 0)
+                return y;
+        if (y == 0)
+                return x;
+
+        while (x > 0) {
+                long t = x;
+                x = y % x;
+                y = t;
+        }
+        return y;
+}
+
